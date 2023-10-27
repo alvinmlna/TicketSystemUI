19e6af236e285ac98e360c95a52b7b1b3471023f
Alvin Maulana <alvinmaulananrs@gmail.com>
2023-10-28 03:27:14 +0800

    Complete initial edit ticket design

--- src/app/ticket/ticket.module.ts
+++ src/app/ticket/ticket.module.ts
@@ -6,6 +6,8 @@ import { TicketRoutingModule } from './ticket-routing.module';
 import { TicketAddComponent } from './ticket-add/ticket-add.component';
 import { TicketEditComponent } from './ticket-edit/ticket-edit.component';
 import { DiscussionPanelComponent } from './discussion-panel/discussion-panel.component';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faArchive, faCheckCircle, faReply } from '@fortawesome/free-solid-svg-icons';
 
 @NgModule({
   declarations: [
@@ -18,10 +20,19 @@ import { DiscussionPanelComponent } from './discussion-panel/discussion-panel.co
     CommonModule,
     MySharedModule,
     TicketRoutingModule,
+    FontAwesomeModule,
   ], 
   exports: [
     TicketComponent,
     TicketRoutingModule
   ]
 })
-export class TicketModule { }
+export class TicketModule { 
+  constructor(library: FaIconLibrary){
+    library.addIcons(
+      faReply,
+      faCheckCircle,
+      faArchive
+    )
+  }
+}
