c75a5d1e73741a2cd30fbbaf0887225bed457b31
Alvin Maulana <alvinmaulananrs@gmail.com>
2023-11-04 17:31:34 +0800

    Enabled sidebar by role

--- src/app/core/sidebar/sidebar.component.ts
+++ src/app/core/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CurrentUserService } from '../services/current-user.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,4 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent {
+
+  constructor(private currentUser: CurrentUserService){
+  }
+
+  enableSideBar(title : string){
+    var user = this.currentUser.getUser();
+    if(user) {
+      if(user.roleId == "1")
+      {
+        return this.customerMenu(title);
+      }
+        else if (user.roleId == "2")
+      {
+        return this.adminMenu(title);
+      }
+    } 
+    return false;
+  }
+
+  customerMenu(title : string) {
+    var menu = ['ticket'];
+    return menu.includes(title);
+  }
+
+  adminMenu(title : string) {
+    var menu = ['ticket', 'dashboard'];
+    return menu.includes(title);
+  }
+
 }
